aa80f31a2c4c583b23b32b24d3f61e883592f77d
ArulMozhiVaradan <jarul@gmail.com>
2020-05-02 00:47:05 +0530

    Meal_item widget added and project structure changed

--- lib/screens/category_meals_screen.dart
+++ lib/screens/category_meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import './dummy_data.dart';
+import 'package:meals/widgets/meal_item.dart';
+import '../dummy_data.dart';
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
   // final String categoryId;
@@ -17,7 +18,12 @@ class CategoryMealsScreen extends StatelessWidget {
         appBar: AppBar(title:Text(categoryTitle)),
           body: Center(
         child: ListView.builder(itemBuilder: (ctx,index){
-          return Text(categoryMeals[index].title);
+          return MealItem(
+            title: categoryMeals[index].title, 
+            affordability: categoryMeals[index].affordability, 
+            complexity: categoryMeals[index].complexity, 
+            duration: categoryMeals[index].duration, 
+            imageUrl: categoryMeals[index].imageUrl);
         },itemCount: categoryMeals.length,),
       ),
     );
