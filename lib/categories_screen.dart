d5b4976f444f094d04f2f0a50f805715004c1eab
ArulMozhiVaradan <jarul@gmail.com>
2020-05-01 20:23:52 +0530

    GridView is Created

---
+++ lib/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:meals/category_item.dart';
+import './dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Meals App'),
+      ),
+      body: GridView(
+        children: DUMMY_CATEGORIES.map((c) {
+          return CategoryItem(c.title, c.color);
+        }).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200, //maxWidth
+            childAspectRatio: 3 / 2, //maxHeightBasedOnMaxWidth
+            crossAxisSpacing: 20,//VerticalSpacing
+            mainAxisSpacing: 20//HorizontalSpacing
+            ),
+      ),
+    );
+  }
+}
